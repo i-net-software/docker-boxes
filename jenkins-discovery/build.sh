5b295e4c1472734ea471d7279d574d5d44ba4cf7
Dev <gerry.w@gammaproduction.de>
2016-02-22 08:35:03 +0100

    Add index.html for Nginx to display the running contexts Update build.sh to automaticaly fetch the project and run it with one line of code.

--- jenkins-discovery/build.sh
+++ jenkins-discovery/build.sh
@@ -1,3 +1,46 @@
 #!/bin/bash
 
-docker build -t inetsoftware/jenkins-discovery .
+if [ -z $(which docker-compose) ]; then
+    echo "You have to install 'docker-compose'"
+    echo "e.g using: pip install docker-compose"
+    echo
+fi
+
+if [ -z $(which git) ]; then
+    echo "You have to install 'git'"
+    echo "e.g using: apt-get install git"
+    echo
+fi
+
+NEEDS_CLEANUP=0
+CHECKOUT_ROOT=/tmp/docker-boxes
+DOCKER_COMPOSE_OPTS=
+if [ ! -f "./docker-compose.yml" ]; then
+    NEEDS_CLEANUP=1
+
+    echo "Needs to clone repository first"
+    # if the compose is not here, clone it (empty)
+    git clone -n https://github.com/i-net-software/docker-boxes.git --depth 1 "$CHECKOUT_ROOT"
+    cd "$CHECKOUT_ROOT"
+    # checkout the subdirectory, we only need this
+    git checkout HEAD jenkins-discovery
+    
+    # cd back and forth to preserver the history
+    cd -
+    DOCKER_COMPOSE_OPTS="-p $CHECKOUT_ROOT/jenkins-discovery"
+fi
+
+HOST_ADDRESS=$(hostname -I | awk '{print $1}')
+
+if [ -z $0 ]; then
+    docker-compose up -d
+    echo "Started the docker-compose.yml content as daemon."
+    echo "Check with: 'docker ps'"
+else
+    docker-compose $DOCKER_COMPOSE_OPTS $@
+fi
+
+if [ $NEEDS_CLEANUP == 1 ]; then
+    echo "Cleaning up after me"
+    rm -rf "$CHECKOUT_ROOT"
+fi
