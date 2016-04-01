33c38ec808ec71c7b3b149eb14dd14b45f0089a5
Gerry Weißbach <gerry.w@gammaproduction.de>
2016-04-01 10:58:36 +0200

    install phantomjs via download.

---
+++ build-slaves/ubuntu/ubuntu-x11-firefox/install-phantomjs.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# This script install PhantomJS in your Debian/Ubuntu System
+#
+# This script must be run as root:
+# sudo sh install_phantomjs.sh
+#
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
+PHANTOM_VERSION="phantomjs-1.9.8"
+ARCH=$(uname -m)
+
+if ! [ $ARCH = "x86_64" ]; then
+	$ARCH="i686"
+fi
+
+PHANTOM_JS="$PHANTOM_VERSION-linux-$ARCH"
+
+cd ~
+wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
+sudo tar xvjf $PHANTOM_JS.tar.bz2
+
+sudo mv $PHANTOM_JS /usr/local/share
+sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
