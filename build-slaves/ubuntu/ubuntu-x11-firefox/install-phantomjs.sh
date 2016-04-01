b80cf920a91585aa8db0691449352391431687ee
Gerry Weißbach <gerry.w@gammaproduction.de>
2016-04-01 10:59:55 +0200

    Some corrections.

--- build-slaves/ubuntu/ubuntu-x11-firefox/install-phantomjs.sh
+++ build-slaves/ubuntu/ubuntu-x11-firefox/install-phantomjs.sh
@@ -19,6 +19,14 @@ fi
 
 PHANTOM_JS="$PHANTOM_VERSION-linux-$ARCH"
 
+sudo apt-get update
+sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y
+sudo apt-get install libfreetype6 libfreetype6-dev -y
+sudo apt-get install libfontconfig1 libfontconfig1-dev -y
+sudo apt-get autoclean
+sudo apt-get autoremove
+sudo rm -rf /var/lib/apt/lists/*
+
 cd ~
 wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
 sudo tar xvjf $PHANTOM_JS.tar.bz2
