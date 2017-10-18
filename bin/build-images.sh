88fba7c022e99eb49034fbd46d09bdcdd9ff952e
Gerry Weißbach <gerry.w@gammaproduction.de>
2017-10-18 07:58:12 +0200

    Add pull for the base images.

--- bin/build-images.sh
+++ bin/build-images.sh
@@ -44,7 +44,7 @@ if [ -z "$COMMAND" ] || [ "build" == "$COMMAND" ] || [ "push" == "$COMMAND" ]; t
     # build the images if nothing else is set
     for IMG in $IMAGES; do
         echo "Building Image for: $IMG"
-        docker-compose -f "$DOCKERCOMPOSEFILE" build "$@" "$IMG"
+        docker-compose -f "$DOCKERCOMPOSEFILE" build --pull "$@" "$IMG"
     done
 
     for ARTIFACT in $ARTIFACTS; do
