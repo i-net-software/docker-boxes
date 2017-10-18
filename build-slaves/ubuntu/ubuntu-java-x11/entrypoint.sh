a3991a706b866ffe9369272795c07b9a386417c0
Gerry Weißbach <gerry.w@gammaproduction.de>
2017-10-18 13:19:18 +0200

    Partially Back - CMD for simple sshd, ENTRYPOINT for supervisord.

--- build-slaves/ubuntu/ubuntu-java-x11/entrypoint.sh
+++ build-slaves/ubuntu/ubuntu-java-x11/entrypoint.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
+
 set -e
+set -u
 
-if [ "$1" = 'daemon' ]; then
-    exec /usr/bin/supervisord -n
-    exit 0
+# Supervisord default params
+SUPERVISOR_PARAMS='-c /etc/supervisor/supervisord.conf'
+
+# We have TTY, so probably an interactive container...
+if test -t 0; then
+  # Run supervisord detached...
+  /usr/bin/supervisord $SUPERVISOR_PARAMS
+  
+  # Some command(s) has been passed to container? Execute them and exit.
+  # No commands provided? Run bash.
+  if [[ $@ ]]; then 
+    eval $@
+  else 
+    export PS1='[\u@\h : \w]\$ '
+    /bin/bash
+  fi
+
+# Detached mode? Run supervisord in foreground, which will stay until container is stopped.
+else
+  # If some extra params were passed, execute them before.
+  # @TODO It is a bit confusing that the passed command runs *before* supervisord, 
+  #       while in interactive mode they run *after* supervisor.
+  #       Not sure about that, but maybe when any command is passed to container,
+  #       it should be executed *always* after supervisord? And when the command ends,
+  #       container exits as well.
+  if [[ $@ ]]; then 
+    eval $@
+  fi
+  /usr/bin/supervisord -n $SUPERVISOR_PARAMS
 fi
-
-exec /usr/bin/supervisord
-
-exec "$@"
