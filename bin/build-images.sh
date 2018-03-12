f3ff91f6dc4fa2fdc6ff92a7976a1dadfc7e8b82
Gerry Weißbach <gweissbach@inetsoftware.de>
2018-03-12 08:51:30 +0100

    Update image version to v4

--- bin/build-images.sh
+++ bin/build-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG="v3"
+TAG="v4"
 GITBRANCH=`git rev-parse --abbrev-ref HEAD`
 ROOT=$(eval $(printf "%s -f %s/ | xargs dirname" $([ ! -z $(which greadlink) ] && echo readlink | echo greadlink) $(dirname $0)))
 
