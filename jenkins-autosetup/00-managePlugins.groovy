322da18672123df32e39c4b647c8d503fe332fa2
Gerry Weißbach <gerry.w@gammaproduction.de>
2021-04-16 12:35:12 +0200

    Only automatically update latest
    If a fixed version number was given, an update is not performed. That may result in a non-working condition due to newer depencenies in a newer jenkins, but it makes sure that only the fixed plugin version from the plugin.txt will be used.

--- jenkins-autosetup/00-managePlugins.groovy
+++ jenkins-autosetup/00-managePlugins.groovy
@@ -4,31 +4,33 @@ import java.util.logging.Logger
 
 def logger = Logger.getLogger("")
 def installed = false
-def initialized = false
 
+def env = System.getenv()
 def pluginParameter = new File('/usr/share/jenkins/ref/plugins.txt').text
 def plugins = pluginParameter.split()
 logger.info("" + plugins)
 def instance = Jenkins.getInstance()
 def pm = instance.getPluginManager()
 def uc = instance.getUpdateCenter()
-uc.updateAllSites()
+def updateJenkins = !!(env['UPDATE_JENKINS']?:false)
 
+logger.info("Automatic update of fixed Jenkins Plugin versions to 'latest' is " + (updateJenkins?'enabled':'disabled') + '.')
+def shouldUpdate = { plugin, checkVersion ->
+    return ( updateJenkins && plugin.isOlderThan(checkVersion) ) || checkVersion.toString()=="latest"
+}
+
+uc.updateAllSites()
 plugins.each {
     def (pluginName, version) = it.tokenize( ':' )
     def checkVersion = new VersionNumber(!version?"latest":version)
   
-    logger.info("Checking " + pluginName + " Version:" + checkVersion)
+    logger.info("Checking '" + pluginName + "' Version: '" + checkVersion + "'")
     def pl = pm.getPlugin(pluginName);
-    if (!pl || (pl.hasUpdate() && ( pl.isOlderThan(checkVersion) || checkVersion.toString()=="latest" ) ) ) {
+    if (!pl || (pl.hasUpdate() && shouldUpdate( pl, checkVersion) ) ) {
         logger.info("\tLooking at UpdateCenter for: " + pluginName)
-        if (!initialized) {
-            uc.updateAllSites()
-            initialized = true
-        }
 
-        def plugin = uc.getPlugin(pluginName)
-        if (plugin) {
+        def plugin = uc.getPlugin(pluginName, checkVersion)
+        if (plugin && shouldUpdate( plugin, checkVersion) ) {
             logger.info( "\t" +(pl && pl.hasUpdate()?"Updating ":"Installing ") + pluginName)
             def exec = plugin.deploy()
             while( !exec.isDone() ) {
@@ -37,6 +39,8 @@ plugins.each {
 
             logger.info( "\tDone for " + pluginName)
             installed = true
+        } else {
+            logger.info( "\tDid not update " + pluginName )
         }
     }
 }
