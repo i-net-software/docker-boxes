fffab514f86073fe070198e498cec1efb15de027
Gerry Weißbach <gerry.w@gammaproduction.de>
2024-11-05 11:03:21 +0100

    Version is not required anymore. This removes a warning.

--- bin/build-images.sh
+++ bin/build-images.sh
@@ -64,7 +64,7 @@ if [ -z "$COMMAND" ] || [ "build" == "$COMMAND" ] || [ "push" == "$COMMAND" ]; t
     # build the images if nothing else is set
     for IMG in $IMAGES; do
         echo "Building Image for: $IMG"
-        docker-compose -f "$DOCKERCOMPOSEFILE" build $REGISTRY_ARG "$@" "$IMG"
+        docker-compose -f "$DOCKERCOMPOSEFILE" build --no-cache $REGISTRY_ARG "$@" "$IMG"
         if [ $? -ne 0 ]; then
             echo "ERROR - could not build image"
             break;
